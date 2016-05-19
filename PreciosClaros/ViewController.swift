5f12d03ce243b9ff1c5d0ef53d45222e5c1ceff2
Mariano Molina <xsin.crueldadx@gmail.com>
2016-05-19 00:01:57 -0300

    Initial Commit

---
+++ PreciosClaros/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PreciosClaros
+//
+//  Created by Mariano Molina on 19/5/16.
+//  Copyright © 2016 marianomolina.xyz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
